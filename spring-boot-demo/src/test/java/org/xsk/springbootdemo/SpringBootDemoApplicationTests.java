b454f73c5ce3ff8608504259d91b96896e1fbf14
X-one <18236651212@163.com>
2020-11-15 12:04:15 +0800

    create spring boot project

---
+++ spring-boot-demo/src/test/java/org/xsk/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xsk.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
