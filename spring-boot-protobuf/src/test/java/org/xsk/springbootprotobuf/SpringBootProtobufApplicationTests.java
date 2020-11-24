0b5b39dfdf8e4bd022c712a6de6a0a16665fec37
X-one <18236651212@163.com>
2020-11-24 23:38:08 +0800

    spring boot + protobuf demo project

---
+++ spring-boot-protobuf/src/test/java/org/xsk/springbootprotobuf/SpringBootProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xsk.springbootprotobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProtobufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
